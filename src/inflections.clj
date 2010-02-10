5ba0e5a10ee294b7f1df3a7fdef4bc9e5605cdbb
Roman Scherer <roman.scherer@burningswell.com>
2010-02-10 21:04:39 +0100

    Uncountable words can be defined and recognized.

--- src/inflections.clj
+++ src/inflections.clj
@@ -1,11 +1,37 @@
 (ns inflections
   (:require [clojure.contrib.str-utils2 :as str2]))
 
-(def *uncountable-words* (atom []))
+(def *uncountable-words* (atom {}))
+
+(defn- normalize-word [word]
+  (str2/lower-case (str2/trim (if (symbol? word) (name word) word))))
+
+(defn- inflect-uncountable [word]
+  (let [normalized-word (normalize-word word)]
+    (if-not (@*uncountable-words* normalized-word)
+      (swap! *uncountable-words* assoc normalized-word word))))
+
+(defmacro uncountable [& words]
+  (doseq [word words] (inflect-uncountable word)))
 
 (defn dasherize [word]
   (str2/replace word #"_" "-"))
 
+(defn uncountable? [word]
+  (not (nil? (@*uncountable-words* (normalize-word word)))))
+
 (defn underscore [word]
   (str2/replace word #"[-\s]+" "_"))
+
+(uncountable
+    air alcohol art blood butter cheese chewing coffee
+    confusion cotton education electricity entertainment equipment
+    experience fiction fish food forgiveness fresh gold gossip grass
+    ground gum happiness history homework honey ice information jam
+    knowledge lightning liquid literature love luck luggage meat milk
+    mist money music news oil oxygen paper patience peanut pepper
+    petrol pork power pressure research rice sadness series sheep
+    shopping silver snow space species speed steam sugar sunshine tea
+    tennis thunder time toothpaste traffic up vinegar washing wine
+    wood wool)
 
--- test/test/inflections.clj
+++ test/test/inflections.clj
@@ -5,6 +5,11 @@
   (are [word expected] (= (dasherize word) expected)
        "puni_puni" "puni-puni"))
 
+(deftest test-uncountable?
+  (is (every? uncountable? (keys @*uncountable-words*)))
+  (are [word] (is (= (uncountable? word) true))
+       "air" "alcohol" "art" "blood" "butter" "cheese"))
+
 (deftest test-underscore
   (are [word expected] (= (underscore word) expected)
        "puni-puni" "puni_puni"
