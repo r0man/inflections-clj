e224c8845c5ae3937148b8baf50d7ee9d2f0d016
Roman Scherer <roman.scherer@burningswell.com>
2010-02-10 20:35:14 +0100

    Added leiningen project file.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject inflections "0.1-SNAPSHOT"
+  :description "Clojure Inflection Library."
+  :url "http://github.com/r0man/inflections-clj"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
+                     [swank-clojure "1.1.0"]])
