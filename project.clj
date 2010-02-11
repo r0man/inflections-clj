f636d6a4cba72ada45fbbd40a78b452f7b3aa5ad
Roman Scherer <roman.scherer@burningswell.com>
2010-02-11 10:58:22 +0100

    Added autodoc to development dependencies.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://github.com/r0man/inflections-clj"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
+  :dev-dependencies [[autodoc "0.7.0"]
+                     [lein-clojars "0.5.0-SNAPSHOT"]
                      [swank-clojure "1.1.0"]])
