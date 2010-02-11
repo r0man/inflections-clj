652b08a3f80ff901f79a8a22bbb07d03b7bb91b5
Roman Scherer <roman.scherer@burningswell.com>
2010-02-11 19:51:37 +0100

    Refactored irregular words.

--- src/inflections/irregular.clj
+++ src/inflections/irregular.clj
@@ -1,37 +1,56 @@
 (ns inflections.irregular
-  (:use clojure.contrib.seq-utils inflections.helper))
+  (:use clojure.contrib.seq-utils
+        inflections.helper
+        inflections.plural
+        inflections.uncountable
+        ))
 
-(def *irregular-words* (atom {}))
+(def *irregular-words* (atom (set [])))
 
-(defn irregular! [singular plural]
-  (let [singular (normalize-word singular)]
-    (if-not (@*irregular-words* singular)
-      (swap! *irregular-words* assoc singular (normalize-word plural)))))
+(defn add-irregular-word
+  "Adds a the given word to the list of irregular words."
+  [word]
+  (swap! *irregular-words* conj (normalize-word word)))
+
+(defn delete-irregular-word
+  "Deletes the given word from the list of irregular words."
+  [word]
+  (swap! *irregular-words* disj (normalize-word word)))
+
+(defn reset-irregular-words
+  "Resets the set of irregular words."
+  []
+  (reset! *irregular-words* (set [])))
 
 (defn irregular?
   "Returns true if the given word is irregular, else false."
   [word]
-  (let [word (normalize-word word)]
-    (or (includes? (keys @*irregular-words*) word)
-        (includes? (vals @*irregular-words*) word))))
+  (contains? @*irregular-words* (normalize-word word)))
 
-(defmacro irregular [& words]
-  (if-not (= (mod (count words) 2) 0)
-    (throw (IllegalArgumentException. "Odd number of words given. Pairs of singular and plural words expected.")))
-  (doseq [[singular plural] (partition 2 words)]
-    (irregular! singular plural)))
+(defn irregular
+  "Define words that are irregular in singular and plural."
+  [& singulars-and-plurals]
+  (assert-even-args singulars-and-plurals)
+  (doseq [[singular plural] (partition 2 singulars-and-plurals)]
+    (delete-uncountable-word singular)
+    (delete-uncountable-word plural)
+    (add-irregular-word singular)
+    (add-irregular-word plural)))
 
-(irregular
- child children
- cow kine
- foot feet
- louse lice
- mailman mailmen
- man men
- mouse mice
- move moves
- ox oxen
- person people
- sex sexes
- tooth teeth
- woman women)
+(defn init-irregular-words []
+  (reset-irregular-words)
+  (irregular
+   "child" "children"
+   "cow" "kine"
+   "foot" "feet"
+   "louse" "lice"
+   "mailman" "mailmen"
+   "man" "men"
+   "mouse" "mice"
+   "move" "moves"
+   "ox" "oxen"
+   "person" "people"
+   "sex" "sexes"
+   "tooth" "teeth"
+   "woman" "women"))
+
--- test/inflections/test/irregular.clj
+++ test/inflections/test/irregular.clj
@@ -1,10 +1,49 @@
 (ns inflections.test.irregular
   (:use clojure.test inflections.irregular))
 
+(deftest test-add-irregular-word
+  (reset-irregular-words)
+  (add-irregular-word "man")
+  (is (= (seq @*irregular-words*) ["man"]))
+  (add-irregular-word "men")
+  (is (= (seq @*irregular-words*) ["men" "man"])))
+
+(deftest test-delete-irregular-word
+  (reset-irregular-words)
+  (add-irregular-word "man")
+  (is (= (seq @*irregular-words*) ["man"]))
+  (delete-irregular-word "man")
+  (is (nil?  (seq @*irregular-words*))))
+
+(deftest test-reset-irregular-words
+  (add-irregular-word "man")
+  (reset-irregular-words)
+  (is (nil?  (seq @*irregular-words*))))
+
+(deftest test-irregular-with-single-rule
+  (reset-irregular-words)
+  (irregular "child" "children")
+  (is (= @*irregular-words* #{"child" "children"})))
+
+(deftest test-irregular-with-multiple-rule
+  (reset-irregular-words)
+  (irregular "child" "children" "cow" "kine")
+  (is (= @*irregular-words* #{"cow" "kine" "child" "children"})))
+
 (deftest test-irregular?
-  (is (every? irregular? (keys @*irregular-words*)))
-  (is (every? irregular? (vals @*irregular-words*)))
-  (are [word]
-       (is (= (irregular? word) true))
-       "person" "people" "man" "men" "child" "children"))
+  (init-irregular-words)
+  (are [word] (is (irregular? word))
+       "child" "children"
+       "cow" "kine"
+       "foot" "feet"
+       "louse" "lice"
+       "mailman" "mailmen"
+       "man" "men"
+       "mouse" "mice"
+       "move" "moves"
+       "ox" "oxen"
+       "person" "people"
+       "sex" "sexes"
+       "tooth" "teeth"
+       "woman" "women"))
 
