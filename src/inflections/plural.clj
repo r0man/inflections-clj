95e02275099e3cbf5aba6e82f75090bd78cc53f8
Roman Scherer <roman.scherer@burningswell.com>
2010-02-11 19:52:11 +0100

    Refactored plural rules.

---
+++ src/inflections/plural.clj
@@ -0,0 +1,87 @@
+(ns inflections.plural
+  (:refer-clojure :exclude (replace))
+  (:use [clojure.contrib.str-utils2 :only (blank? replace)]
+        clojure.contrib.seq-utils
+        inflections.helper
+        inflections.uncountable))
+
+(def *plural-rules* (atom []))
+
+(defstruct rule :pattern :replacement)
+
+(defn make-rule [pattern replacement]
+  (struct rule pattern replacement))
+
+(defn add-plural-rule
+  "Adds the given pattern and replacement to the pluralization rules."
+  [pattern replacement]
+  (let [rule (make-rule pattern replacement)]
+    (if-not (includes? @*plural-rules* rule)
+      (swap! *plural-rules* conj rule))))
+
+(defn reset-plural-rules
+  "Resets the list of plural rules."
+  [] (reset! *plural-rules* []))
+
+(defn plural
+  "Define rule(s) to map from singular to plural."
+  [& patterns-and-replacements]
+  (assert-even-args patterns-and-replacements)
+  (doseq [[pattern replacement] (partition 2 patterns-and-replacements)]
+    (add-plural-rule pattern replacement)))
+
+(defn pluralize
+  "Returns the plural of the given word."
+  [word]
+  (if (or (blank? word) (uncountable? word))
+    word
+    (first (apply-rules (rseq @*plural-rules*) word))))
+
+(defn init-plural-rules []
+  (reset-plural-rules)
+  (plural
+   #"$" "s"
+   #"s$" "s"
+   #"(ax|test)is$" "$1es"
+   #"(octop|vir)us$" "$1i"
+   #"(alias|status)$" "$1es"
+   #"(bu)s$" "$1ses"
+   #"(buffal|tomat)o$" "$1oes"
+   #"([ti])um$" "$1a"
+   #"sis$" "ses"
+   #"(?:([^f])fe|([lr])f)$" "$1$2ves"
+   #"(hive)$" "$1s"
+   #"([^aeiouy]|qu)y$" "$1ies"
+   #"(x|ch|ss|sh)$" "$1es"
+   #"(matr|vert|ind)(?:ix|ex)$" "$1ices"
+   #"([m|l])ouse$" "$1ice"
+   #"^(ox)$" "$1en"
+   #"(quiz)$" "$1zes"))
+
+
+
+;; (defn reset-)
+
+;; (defn plural! [pattern replacement]
+;;   (if (isa? (class pattern) String) (delete-uncountable pattern))
+;;   (delete-uncountable replacement)
+;;   (if-not (includes? @*plural-rules* [pattern replacement])
+;;     (swap! *plural-rules* conj [pattern replacement])))
+
+;; (defmacro plural [& rules]
+;;   (if-not (= (mod (count rules) 2) 0)
+;;     (throw (IllegalArgumentException. "Odd number of words given. Pairs of pattern and replacement expected.")))
+;;   (doseq [[pattern replacement] (partition 2 rules)]
+;;     (plural! pattern replacement)))
+
+;; (defn pluralize
+;;   "Returns the plural of the given word."
+;;   [word]
+;;   (if (or (blank? word) (uncountable? word))
+;;     word
+;;     (first (apply-rules (rseq @*plural-rules*) word))))
+
+;; (rseq @*plural-rules*)
+
+;; (pluralize "woman")
+
---
+++ test/inflections/test/plural.clj
@@ -0,0 +1,124 @@
+(ns inflections.test.plural
+  (:use clojure.test inflections.plural))
+
+(deftest test-add-plural-rule
+  (reset-plural-rules)
+  (add-plural-rule #"$" "s")
+  (is (= (count @*plural-rules*) 1)))
+
+(deftest test-reset-plural-rules
+  (reset-plural-rules)
+  (add-plural-rule #"$" "s")
+  (is (= (count @*plural-rules*) 1))
+  (reset-plural-rules)
+  (is (= (count @*plural-rules*) 0)))
+
+(deftest test-plural-with-single-rule
+  (reset-plural-rules)
+  (plural #"$" "s")
+  (is (= (count @*plural-rules*) 1))
+  (let [rule (first @*plural-rules*)]
+    (is (= (str (:pattern rule)) "$"))
+    (is (= (:replacement rule) "s"))))
+
+(deftest test-plural-with-multiple-rule
+  (reset-plural-rules)
+  (plural #"$" "s" #"s$" "s")
+  (is (= (count @*plural-rules*) 2))
+  (let [rule (first @*plural-rules*)]
+    (is (= (str (:pattern rule)) "$"))
+    (is (= (:replacement rule) "s")))
+  (let [rule (last @*plural-rules*)]
+    (is (= (str (:pattern rule)) "s$"))
+    (is (= (:replacement rule) "s"))))
+
+(deftest test-pluralize
+  (init-plural-rules)
+  (are [word expected]
+       (is (= (pluralize word) expected))
+       " " " " 
+       "" ""
+       "ability" "abilities"
+       "address" "addresses"
+       "agency" "agencies"
+       "alias" "aliases"
+       "analysis" "analyses"
+       "archive" "archives"
+       "axis" "axes"
+       "basis" "bases"
+       "box" "boxes"
+       "buffalo" "buffaloes"
+       "bus" "buses"
+       "case" "cases"
+       "category" "categories"
+       "comment" "comments"
+       "crisis" "crises"
+       "database" "databases"
+       "datum" "data"
+       "day" "days"
+       "diagnosis" "diagnoses"
+       "diagnosis_a" "diagnosis_as"
+       "dwarf" "dwarves"
+       "edge" "edges"
+       "elf" "elves"
+       "experience" "experiences"
+       "fix" "fixes"
+       "foobar" "foobars"
+       "half" "halves"
+       "horse" "horses"
+       "house" "houses"
+       "index" "indices"
+       "louse" "lice"
+       "matrix" "matrices"
+       "matrix_fu" "matrix_fus"
+       "medium" "media"
+       "mouse" "mice"
+       "move" "moves"
+       "movie" "movies"
+       "newsletter" "newsletters"
+       "octopus" "octopi"
+       "ox" "oxen"
+       "perspective" "perspectives"
+       "photo" "photos"
+       "portfolio" "portfolios"
+       "prize" "prizes"
+       "process" "processes"
+       "query" "queries"
+       "quiz" "quizzes"
+       "safe" "saves"
+       "search" "searches"
+       "shoe" "shoes"
+       "stack" "stacks"
+       "status" "statuses"
+       "status_code" "status_codes"
+       "switch" "switches"
+       "testis" "testes"
+       "tomato" "tomatoes"
+       "vertex" "vertices"
+       "virus" "viri"
+       "wife" "wives"
+       "wish" "wishes"))
+
+(deftest test-pluralize-with-irregular-words
+  (init-plural-rules)
+  (are [word expected]
+       (is (= (pluralize word) expected))
+       ;; "child" "children"
+       ;; "cow" "kine"
+       ;; "woman" "women"
+       ;; "salesperson" "salespeople"
+       ;; "series" "series"
+       ;; "species" "species"
+       ;; "spokesman" "spokesmen"
+       ;; "news" "news"
+       ;; "node_child" "node_children"
+       ;; "old_news" "old_news"
+       ;; "person" "people"
+       ;; "rice" "rice"
+       ;; "equipment" "equipment"
+       ;; "fish" "fish"
+       ;; "information" "information"
+       ;; "man" "men"
+       ))
+
+
