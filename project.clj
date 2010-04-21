3484ec8b671de08d12370b69391ff567abd4e8bc
Roman Scherer <roman@soundcloud.com>
2010-04-21 20:02:32 +0200

    Updated project.clj.

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
 (defproject inflections "0.3"
   :author "Roman Scherer"
+  :autodoc {:copyright "Copyright (c) 2010 Roman Scherer" :name "inflections-clj"}
   :description "Rails-like inflections for Clojure."
   :url "http://github.com/r0man/inflections-clj"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
   :dev-dependencies [[autodoc "0.7.0"]
-                     [lein-clojars "0.5.0-SNAPSHOT"]
-                     [swank-clojure "1.1.0"]]
-  :autodoc {:copyright "Copyright (c) 2010 Roman Scherer"
-            :name "inflections-clj"})
+                     [lein-clojars "0.5.0"]
+		     [leiningen/lein-swank "1.1.0"]])
