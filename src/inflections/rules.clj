0ec0826445b45d267d7ddcb82d7e114d364820d5
Roman Scherer <roman@soundcloud.com>
2010-05-30 16:41:26 +0200

    Adapted rules to clojure 1.2.

--- src/inflections/rules.clj
+++ src/inflections/rules.clj
@@ -1,9 +1,16 @@
 (ns inflections.rules
-  (:use [clojure.contrib.string :only (replace-re)]
+  (:refer-clojure :exclude [replace])
+  (:import java.util.regex.Pattern)
+  (:use [clojure.contrib.string :only (replace-re replace-str)]
         [clojure.contrib.seq-utils :only (includes?)]
         inflections.helper))
 
 (defstruct rule :pattern :replacement)
+
+(defn- replace [pattern replacement s]
+  (if (isa? (class pattern) Pattern)
+    (replace-re pattern replacement s)
+    (replace-str pattern replacement s)))
 
 (defn add-rule! [rules rule]
   (if-not (includes? (deref rules) rule)
@@ -19,8 +26,8 @@
   (assert-even-args patterns-and-replacements)
   (map #(apply make-rule %) (partition 2 patterns-and-replacements)))
 
-(defn resolve-rule [rule word]  
-  (let [inflection (replace-re (:pattern rule) (:replacement rule) word)]
+(defn resolve-rule [rule word]
+  (let [inflection (replace (:pattern rule) (:replacement rule) word)]
     (if-not (= inflection word)
       inflection)))
 
@@ -32,6 +39,5 @@
   [rules] (reset! rules []))
 
 (defmacro with-reset-rules [rules & body]
-  `(do
-     (reset-rules! ~rules)
-     ~@body))
+  `(do (reset-rules! ~rules)
+       ~@body))
