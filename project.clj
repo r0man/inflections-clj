e5ec578bee560b6e04c4eea08fd2f97db6f6eabd
Roman Scherer <roman@soundcloud.com>
2010-06-02 16:12:17 +0200

    Using newer autodoc.

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
   :url "http://github.com/r0man/inflections-clj"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
-  :dev-dependencies [[autodoc "0.7.0"]
+  :dev-dependencies [[autodoc "0.7.1"]
                      [lein-clojars "0.5.0"]
                      [swank-clojure "1.2.1"]])
