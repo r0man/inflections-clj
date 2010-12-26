9d8966bf55cabaf8aff0041715fcfb680ae73911
roman <roman@soundclaat.localdomain>
2010-12-26 17:04:00 +0100

    Upgraded to clojure 1.2.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :autodoc {:copyright "Copyright (c) 2010 Roman Scherer" :name "inflections"}
   :description "Rails-like inflections for Clojure."
   :url "http://github.com/r0man/inflections-clj"
-  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
-                 [org.clojure/clojure-contrib "1.2.0-RC1"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[autodoc "0.7.1"]
                      [lein-clojars "0.5.0"]
                      [swank-clojure "1.2.1"]])
