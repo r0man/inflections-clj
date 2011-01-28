e9f22b57c435b29b77284938ed8cae0dd9519749
Roman <roman@burningswell.com>
2011-01-28 12:13:04 +0100

    Autodoc.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject inflections "0.4-SNAPSHOT"
+(defproject inflections "0.4.1-SNAPSHOT"
   :author "Roman Scherer"
   :autodoc {:copyright "Copyright (c) 2010 Roman Scherer" :name "inflections"}
   :description "Rails-like inflections for Clojure."
   :url "http://github.com/r0man/inflections-clj"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"]
+  :dev-dependencies [[autodoc "0.7.1"]
                      [swank-clojure "1.2.1"]])
