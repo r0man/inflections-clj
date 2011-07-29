168c5ba1eb53c9f522f9224988d61433500e511b
Roman <roman@burningswell.com>
2011-07-29 12:08:45 +0200

    Started 0.5.1-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject inflections "0.5.0"
+(defproject inflections "0.5.1-SNAPSHOT"
   :author "Roman Scherer"
   :autodoc {:copyright "Copyright (c) 2011 Roman Scherer" :name "inflections"}
   :description "Rails-like inflections for Clojure."
