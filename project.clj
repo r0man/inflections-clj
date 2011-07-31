009207a1507462b3f73600c0f7f630e012606bc9
Roman <roman@burningswell.com>
2011-07-31 14:45:10 +0200

    Added lein-multi.

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :autodoc {:copyright "Copyright (c) 2011 Roman Scherer" :name "inflections"}
   :description "Rails-like inflections for Clojure."
   :url "http://github.com/r0man/inflections-clj"
-  :dependencies [[org.clojure/clojure "1.3.0-beta1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :multi-deps {"1.3.0-beta1" [[org.clojure/clojure "1.3.0-beta1"]]})
