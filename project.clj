64d7254d32e923c372fb1bf9a7842d652f19c57e
Roman <roman@burningswell.com>
2011-09-24 11:09:29 +0200

    Release 0.6.0.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject inflections "0.6.0-SNAPSHOT"
+(defproject inflections "0.6.0"
   :author "Roman Scherer"
   :autodoc {:copyright "Copyright (c) 2011 Roman Scherer" :name "inflections"}
   :description "Rails-like inflections for Clojure."
   :url "http://github.com/r0man/inflections-clj"
-  :dependencies [[org.clojure/clojure "1.3.0-RC0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
   :multi-deps {"1.2.1" [[org.clojure/clojure "1.2.1"]]})
