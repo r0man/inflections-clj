05456e95c21e352127c2bc01e3e8b1efe8a6be8f
Roman <roman@burningswell.com>
2012-05-02 21:30:56 +0200

    Ported uncountable namespace to ClojureScript.

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject inflections "0.7.0-SNAPSHOT"
   :description "Rails-like inflections for Clojure."
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :author "Roman Scherer"
   :url "http://github.com/r0man/inflections-clj"
-  :plugins [[lein-cljsbuild "0.1.3"]]
-  ;; :hooks [leiningen.cljsbuild]
+  :plugins [[lein-cljsbuild "0.1.8"]]
+  :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds [{:source-path "src/cljs"
                         :compiler {:output-to "target/inflections-debug.js"}}
                        {:source-path "src/cljs"
@@ -24,5 +25,7 @@
               :repl-launch-commands
               {"chromium" ["chromium" "test-resources/index.html"]
                "firefox" ["firefox" "test-resources/index.html"]}}
-  :source-paths ["src/clj"]
+  :source-paths ["src/clj"
+                 "checkouts/clojurescript/src/clj"
+                 "checkouts/clojurescript/src/cljs"]
   :test-paths ["test/clj"])
---
+++ src/cljs/inflections/uncountable.cljs
@@ -0,0 +1,36 @@
+(ns inflections.uncountable
+  (:use [clojure.string :only (lower-case)]))
+
+(def ^{:dynamic true} *uncountable-words*
+  (atom (sorted-set)))
+
+(defprotocol Uncountable
+  (add-uncountable! [obj]
+    "Adds obj to the set of *uncountable-words*.")
+  (delete-uncountable! [obj]
+    "Delete obj from the set of *uncountable-words*.")
+  (uncountable? [obj]
+    "Returns true if obj is an uncountable word, otherwise false."))
+
+(extend-type string
+  Uncountable
+  (uncountable? [s]
+    (contains? @*uncountable-words* (lower-case (name s))))
+  (add-uncountable! [s]
+    (swap! *uncountable-words* conj (lower-case (name s))))
+  (delete-uncountable! [s]
+    (swap! *uncountable-words* disj (lower-case (name s)))))
+
+(defn init-uncountable-words []
+  (doall
+   (map add-uncountable!
+        ["air" "alcohol" "art" "blood" "butter" "cheese" "chewing" "coffee"
+         "confusion" "cotton" "education" "electricity" "entertainment" "equipment"
+         "experience" "fiction" "fish" "food" "forgiveness" "fresh" "gold" "gossip" "grass"
+         "ground" "gum" "happiness" "history" "homework" "honey" "ice" "information" "jam"
+         "knowledge" "lightning" "liquid" "literature" "love" "luck" "luggage" "meat" "milk"
+         "mist" "money" "music" "news" "oil" "oxygen" "paper" "patience" "peanut" "pepper"
+         "petrol" "pork" "power" "pressure" "research" "rice" "sadness" "series" "sheep"
+         "shopping" "silver" "snow" "space" "species" "speed" "steam" "sugar" "sunshine" "tea"
+         "tennis" "thunder" "time" "toothpaste" "traffic" "up" "vinegar" "washing" "wine"
+         "wood" "wool"])))
