e4b3f7f635b7e3fbab3143d897c60436dac641a3
Roman <roman@burningswell.com>
2012-05-11 18:04:19 +0200

    Removed checkouts from source-paths.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject inflections "0.7.0-SNAPSHOT"
   :description "Rails-like inflections for Clojure."
+  :url "http://github.com/r0man/inflections-clj"
+  :author "Roman Scherer"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :author "Roman Scherer"
-  :url "http://github.com/r0man/inflections-clj"
   :plugins [[lein-cljsbuild "0.1.9"]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds [{:source-path "src/cljs"
@@ -17,7 +17,5 @@
               :repl-launch-commands
               {"chromium" ["chromium" "test-resources/index.html"]
                "firefox" ["firefox" "test-resources/index.html"]}}
-  :source-paths ["src/clj"
-                 "checkouts/clojurescript/src/clj"
-                 "checkouts/clojurescript/src/cljs"]
+  :source-paths ["src/clj"]
   :test-paths ["test/clj"])
