fc8dbdd935800eea0840e036b62ae3c6940a9fb5
Roman <roman@burningswell.com>
2012-05-11 17:54:52 +0200

    Switched to lein-cljsbuild 0.1.9.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :author "Roman Scherer"
   :url "http://github.com/r0man/inflections-clj"
-  :plugins [[lein-cljsbuild "0.1.8"]]
+  :plugins [[lein-cljsbuild "0.1.9"]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds [{:source-path "src/cljs"
                         :compiler {:output-to "target/inflections-debug.js"}}
