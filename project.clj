e23100205080bd94b38c167a134f0966f347762a
Roman <roman@burningswell.com>
2012-07-06 22:28:03 +0200

    Started 0.7.1-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject inflections "0.7.0-SNAPSHOT"
+(defproject inflections "0.7.1-SNAPSHOT"
   :description "Rails-like inflections for Clojure(Script)."
   :url "http://github.com/r0man/inflections-clj"
   :author "Roman Scherer"
