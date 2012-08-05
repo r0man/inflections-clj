e07c7f4b4eff4ce03d7f53b82a0d0fb8146c69f5
Roman Scherer <roman.scherer@burningswell.com>
2012-08-05 15:19:55 +0200

    Fixed ClojureScript test file.

--- test-cljs.sh
+++ test-cljs.sh
@@ -4,9 +4,9 @@ JS_TESTS="target/inflections-test.js"
 
 [ ! -f $JS_TESTS ] && lein cljsbuild once
 
-if [ -! `type d8 &> /dev/null` ] ; then
+if [ -n "`type d8 2> /dev/null`" ] ; then
     echo "inflections.test.run()" | d8 --shell $JS_TESTS
-elif [ -! `type v8 &> /dev/null` ] ; then
+elif [ -n "`type v8 2> /dev/null`" ] ; then
     echo "inflections.test.run()" | v8 --shell $JS_TESTS
 else
     echo "Can't run ClojureScript tests. Looks like V8 is not installed."
