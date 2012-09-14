118c122e35a6b12d1a146746b17b7e84acabb45c
Jonathan Boston <boston@chgr.mc.vanderbilt.edu>
2012-09-13 20:39:59 -0500

    Started 0.7.4-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject inflections "0.7.3"
+(defproject inflections "0.7.4-SNAPSHOT"
   :description "Rails-like inflections for Clojure(Script)."
   :url "http://github.com/r0man/inflections-clj"
   :author "Roman Scherer"
