005a00d635582234faf4d9a9b8e8579fb8e3c618
Roman Scherer <roman.scherer@burningswell.com>
2013-04-12 16:42:23 +0200

    Release 0.8.1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject inflections "0.8.1-SNAPSHOT"
+(defproject inflections "0.8.1"
   :description "Rails-like inflections for Clojure(Script)."
   :url "http://github.com/r0man/inflections-clj"
   :author "Roman Scherer"
