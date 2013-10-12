33cf0365134fb6b02f4deca5ab99b66c511aefc0
Roman Scherer <roman.scherer@burningswell.com>
2013-10-12 19:08:21 +0200

    lein-release plugin: preparing 0.9.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject inflections "0.9.2-SNAPSHOT"
+(defproject inflections "0.9.2"
   :description "Rails-like inflections for Clojure(Script)."
   :url "http://github.com/r0man/inflections-clj"
   :author "Roman Scherer"
