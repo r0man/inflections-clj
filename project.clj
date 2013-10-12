cf85f0d2719c5bbcbc0eda1e1f20c23d4fab46f0
Roman Scherer <roman.scherer@burningswell.com>
2013-10-12 19:08:11 +0200

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -6,11 +6,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1913"]
-                 [noencore "0.1.5"]]
-  :profiles {:dev {:dependencies [[com.cemerick/clojurescript.test "0.0.4"]
-                                  [com.keminglabs/cljx "0.3.0"]]
-                   :plugins [[com.cemerick/austin "0.1.1"]]
+                 [org.clojure/clojurescript "0.0-1934"]
+                 [noencore "0.1.6"]]
+  :profiles {:dev {:dependencies [[com.keminglabs/cljx "0.3.0"]]
+                   :plugins [[com.cemerick/austin "0.1.1"]
+                             [com.cemerick/clojurescript.test "0.1.0"]]
                    :repl-options {:nrepl-middleware [cljx.repl-middleware/wrap-cljx]}}}
   :plugins [[com.keminglabs/cljx "0.3.0"]
             [lein-cljsbuild "0.3.3"]]
@@ -27,7 +27,7 @@
                   {:source-paths ["test"]
                    :output-path "target/test-classes"
                    :rules :cljs}]}
-  :cljsbuild {:test-commands {"phantom" ["runners/phantomjs.js" "target/testable.js"]}
+  :cljsbuild {:test-commands {"phantom" ["phantomjs" :runner "target/testable.js"]}
               :builds [{:source-paths ["target/classes" "target/test-classes"]
                         :compiler {:output-to "target/testable.js"
                                    :libs [""]
