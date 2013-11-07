cedaa521bdad7a582912c773c396c7c0b7bba3c6
Roman Scherer <roman.scherer@burningswell.com>
2013-11-07 10:31:20 +0100

    lein-release plugin: bumped version from 0.9.4 to 0.9.5-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject inflections "0.9.4"
+(defproject inflections "0.9.5-SNAPSHOT"
   :description "Rails-like inflections for Clojure(Script)."
   :url "http://github.com/r0man/inflections-clj"
   :author "Roman Scherer"
