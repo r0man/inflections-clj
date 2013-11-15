505111c7b904d2a89bcf1ca1ff267720442be4f8
Roman Scherer <roman.scherer@burningswell.com>
2013-11-15 17:54:47 +0100

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -6,14 +6,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2014"]
-                 [noencore "0.1.7"]]
+                 [org.clojure/clojurescript "0.0-2030"]
+                 [noencore "0.1.8"]]
   :profiles {:dev {:dependencies [[com.keminglabs/cljx "0.3.1"]]
-                   :plugins [[com.cemerick/austin "0.1.1"]
-                             [com.cemerick/clojurescript.test "0.1.0"]]
+                   :plugins [[com.cemerick/austin "0.1.3"]
+                             [com.cemerick/clojurescript.test "0.2.1"]]
                    :repl-options {:nrepl-middleware [cljx.repl-middleware/wrap-cljx]}}}
-  :plugins [[com.keminglabs/cljx "0.3.0"]
-            [lein-cljsbuild "0.3.4"]]
+  :plugins [[com.keminglabs/cljx "0.3.1"]
+            [lein-cljsbuild "1.0.0-alpha2"]]
   :hooks [cljx.hooks leiningen.cljsbuild]
   :cljx {:builds [{:source-paths ["src"]
                    :output-path "target/classes"
