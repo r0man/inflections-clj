c97c1cd09b05727488d60b27adf91eec699951a2
Roman Scherer <roman.scherer@burningswell.com>
2014-03-25 12:13:31 +0100

    Do not include cljx in dependencies.

--- project.clj
+++ project.clj
@@ -3,35 +3,37 @@
   :url "http://github.com/r0man/inflections-clj"
   :author "Roman Scherer"
   :min-lein-version "2.0.0"
+  :lein-release {:deploy-via :clojars}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2030"]
-                 [noencore "0.1.8"]]
-  :profiles {:dev {:dependencies [[com.keminglabs/cljx "0.3.1"]]
-                   :plugins [[com.cemerick/austin "0.1.3"]
-                             [com.cemerick/clojurescript.test "0.2.1"]]
-                   :repl-options {:nrepl-middleware [cljx.repl-middleware/wrap-cljx]}}}
-  :plugins [[com.keminglabs/cljx "0.3.1"]
-            [lein-cljsbuild "1.0.0-alpha2"]]
-  :hooks [cljx.hooks leiningen.cljsbuild]
-  :cljx {:builds [{:source-paths ["src"]
-                   :output-path "target/classes"
-                   :rules :clj}
-                  {:source-paths ["src"]
-                   :output-path "target/classes"
-                   :rules :cljs}
-                  {:source-paths ["test"]
-                   :output-path "target/test-classes"
-                   :rules :clj}
-                  {:source-paths ["test"]
-                   :output-path "target/test-classes"
-                   :rules :cljs}]}
-  :cljsbuild {:test-commands {"phantom" ["phantomjs" :runner "target/testable.js"]}
-              :builds [{:source-paths ["target/classes" "target/test-classes"]
-                        :compiler {:output-to "target/testable.js"
-                                   :libs [""]
-                                   :optimizations :advanced
-                                   :pretty-print true}}]}
-  :test-paths ["target/test-classes"]
-  :lein-release {:deploy-via :clojars})
+                 [noencore "0.1.12"]]
+  :cljsbuild {:builds []}
+  :cljsbuild {:builds []}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]
+                                  [org.clojure/clojurescript "0.0-2173"]]
+                   :repl-options {:nrepl-middleware [cljx.repl-middleware/wrap-cljx]}
+                   :plugins [[com.keminglabs/cljx "0.3.2"] ;; Must be before Austin: https://github.com/cemerick/austin/issues/37
+                             [com.cemerick/clojurescript.test "0.3.0"]
+                             [com.cemerick/austin "0.1.3"]
+                             [lein-cljsbuild "1.0.2"]]
+                   :hooks [cljx.hooks leiningen.cljsbuild]
+                   :cljx {:builds [{:source-paths ["src"]
+                                    :output-path "target/classes"
+                                    :rules :clj}
+                                   {:source-paths ["src"]
+                                    :output-path "target/classes"
+                                    :rules :cljs}
+                                   {:source-paths ["test"]
+                                    :output-path "target/test-classes"
+                                    :rules :clj}
+                                   {:source-paths ["test"]
+                                    :output-path "target/test-classes"
+                                    :rules :cljs}]}
+                   :cljsbuild {:test-commands {"node" ["node" :node-runner "target/testable.js"]
+                                               "phantom" ["phantomjs" :runner "target/testable.js"]}
+                               :builds [{:source-paths ["target/classes" "target/test-classes"]
+                                         :compiler {:output-to "target/testable.js"
+                                                    :optimizations :advanced
+                                                    :pretty-print true}}]}
+                   :test-paths ["target/test-classes"]}})
