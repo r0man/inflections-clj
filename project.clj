ede1400757933a564256319343d84eb99e2f6c03
Roman Scherer <roman@burningswell.com>
2014-11-23 12:55:53 +0100

    Version 0.9.14-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject inflections "0.9.13"
+(defproject inflections "0.9.14-SNAPSHOT"
   :description "Rails-like inflections for Clojure(Script)."
   :url "http://github.com/r0man/inflections-clj"
   :author "r0man"
