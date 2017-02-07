26b1af2c622cb24fedcd02648435c72e6c9b389f
r0man <roman@burningswell.com>
2017-02-07 20:25:50 +0100

    Bump version to 0.13.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject inflections "0.12.3-SNAPSHOT"
+(defproject inflections "0.13.0-SNAPSHOT"
   :description "Rails-like inflections for Clojure(Script)."
   :url "http://github.com/r0man/inflections-clj"
   :author "r0man"
