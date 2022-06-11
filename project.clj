041758f2f3b2372586f561475d32b6d955d4f363
r0man <roman@burningswell.com>
2022-06-11 10:07:42 +0200

    Version 0.14.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject inflections "0.14.0-SNAPSHOT"
+(defproject inflections "0.14.0"
   :description "Rails-like inflections for Clojure(Script)."
   :url "http://github.com/r0man/inflections-clj"
   :author "r0man"
