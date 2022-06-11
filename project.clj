bf7994895521fd1177975a90fba0d4ca639207ee
r0man <roman@burningswell.com>
2022-06-11 10:01:15 +0200

    Update dependencies (#28)
    * Update dependencies
    
    * Fix tests

--- project.clj
+++ project.clj
@@ -5,30 +5,35 @@
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/clojurescript "1.11.51" :scope "provided"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :aliases {"ci" ["do"
                   ["test"]
-                  ["doo" "phantom" "none" "once"]
-                  ["doo" "phantom" "advanced" "once"]
+                  ["doo" "node" "none" "once"]
+                  ["doo" "node" "advanced" "once"]
                   ["lint"]]
             "lint" ["do"  ["eastwood"]]}
-  :cljsbuild {:builds [{:id "none"
+  :cljsbuild {:builds [{:id "advanced"
                         :compiler
                         {:main inflections.test
                          :optimizations :none
-                         :output-dir "target/none"
-                         :output-to "target/none.js"}
+                         :output-dir "target/advanced"
+                         :output-to "target/advanced.js"
+                         :parallel-build true
+                         :pretty-print true
+                         :target :nodejs
+                         :verbose false}
                         :source-paths ["src" "test"]}
-                       {:id "advanced"
+                       {:id "none"
                         :compiler
                         {:main inflections.test
                          :optimizations :advanced
-                         :output-dir "target/advanced"
-                         :output-to "target/advanced.js"}
+                         :output-dir "target/none"
+                         :output-to "target/none.js"
+                         :target :nodejs}
                         :source-paths ["src" "test"]}]}
   :deploy-repositories [["releases" :clojars]]
-  :profiles {:dev {:plugins [[jonase/eastwood "0.3.4"]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.11.54"]]
+                   :plugins [[jonase/eastwood "1.2.3"]
                              [lein-cljsbuild "1.1.7"]
                              [lein-difftest "2.0.0"]
                              [lein-doo "0.1.11"]]}})
