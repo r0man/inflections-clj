d82c610682898f7b13d99a0b53bcd27ae6f975de
r0man <roman@burningswell.com>
2025-08-14 12:45:13 +0200

    Update dependencies (#32)
    * Update workflow
    
    * Udate dependencies
    
    * Update clojure
    
    * Set version to 0.15.0-SNAPSHOT
    
    * Remove broken versions.deps.oc badges
    
    * Remove clojurescipt provided dependency
    
    * Remove java 11
    
    * Tweak dependencies
    
    * Test against java 19 and 21
    
    * Test only against jdk 21

--- project.clj
+++ project.clj
@@ -1,13 +1,12 @@
-(defproject inflections "0.14.3-SNAPSHOT"
+(defproject inflections "0.15.0-SNAPSHOT"
   :description "Rails-like inflections for Clojure(Script)."
   :url "http://github.com/r0man/inflections-clj"
   :author "r0man"
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[noencore "0.3.8"]
-                 [org.clojure/clojure "1.11.3"]
-                 [org.clojure/clojurescript "1.11.132" :scope "provided"]]
+  :dependencies [[noencore "0.4.0"]
+                 [org.clojure/clojure "1.12.1"]]
   :aliases {"ci" ["do"
                   ["test"]
                   ["doo" "node" "none" "once"]
@@ -34,7 +33,8 @@
                          :target :nodejs}
                         :source-paths ["src" "test"]}]}
   :deploy-repositories [["releases" :clojars]]
-  :profiles {:dev {:dependencies [[org.clojure/clojurescript "1.11.132"]]
+  :profiles {:dev {:dependencies [[cider/piggieback "0.6.0"]
+                                  [org.clojure/clojurescript "1.12.42"]]
                    :plugins [[jonase/eastwood "1.2.3"]
                              [lein-cljsbuild "1.1.8"]
                              [lein-difftest "2.0.0"]
